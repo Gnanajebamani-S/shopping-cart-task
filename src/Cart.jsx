1e27a33054a1ae164b318f7ae4917b001d4fe917
Gnanajebamani-S <jebamani435@gmail.com>
2024-03-24 18:02:52 +0530

    anchor tag

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -6,16 +6,16 @@ Cart.propTypes = {
 function Cart({ item, removeFromCart }) {
   return (
     <li>
-      <a className="dropdown-item d-flex justify-content-between">
-        {item.name} - RS.{item.rate}
-        &#x2003;&#x2003;&#x2003;
-        <button
-          className="btn btn-outline-dark mt-auto"
-          onClick={() => removeFromCart(item)}
-        >
-          <i className="bi bi-trash-fill"></i>
-        </button>
-      </a>
+      {/* <a className="dropdown-item d-flex justify-content-between"> */}
+      {item.name} - RS.{item.rate}
+      &#x2003;&#x2003;&#x2003;
+      <button
+        className="btn btn-outline-dark mt-auto"
+        onClick={() => removeFromCart(item)}
+      >
+        <i className="bi bi-trash-fill"></i>
+      </button>
+      {/* </a> */}
     </li>
   );
 }
